031843886a5ca4a138f1969e33586b745521e7ed
Edward7820 <edward910530@gmail.com>
2022-12-06 20:27:56 +0800

    forwarding unit

---
+++ Forwarding_Unit.v
@@ -0,0 +1,40 @@
+module ForwardingUnit(
+    EXrs1_i,
+    EXrs2_i,
+    MEMRegWrite_i,
+    MEMrd_i,
+    WBRegWrite_i,
+    WBrd_i,
+    ForwardA_o,
+    ForwardB_o
+);
+    input[4:0] EXrs1_i;
+    input[4:0] EXrs2_i;
+    input MEMRegWrite_i;
+    input[4:0] MEMrd_i;
+    input WBRegWrite_i;
+    input[4:0] WBrd_i;
+    output[1:0] ForwardA_o;
+    output[1:0] ForwardB_o;
+
+    reg[1:0] ForwardA_reg;
+    reg[1:0] ForwardB_reg;
+
+    always@(*) begin
+        if (MEMRegWrite_i && (MEMrd_i!=0) && (MEMrd_i==EXrs1_i)) begin
+            ForwardA_reg <= 2'b10;
+        end
+        else if (WBRegWrite_i && (WBrd_i!=0) && (WBrd_i==EXrs1_i)) begin
+            ForwardA_reg <= 2'b01;
+        end
+        if (MEMRegWrite_i && (MEMrd_i!=0) && (MEMrd_i==EXrs2_i)) begin
+            ForwardB_reg <= 2'b10;
+        end
+        else if (WBRegWrite_i && (WBrd_i!=0) && (WBrd_i==EXrs2_i)) begin
+            ForwardB_reg <= 2'b01;
+        end 
+    end
+    assign ForwardA_o = ForwardA_reg;
+    assign ForwardB_o = ForwardB_reg;
+
+endmodule
